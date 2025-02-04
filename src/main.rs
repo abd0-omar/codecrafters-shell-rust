c47a26b5792e5684d3e3fb20b1b2c5e1ba7cbba4
abdo <abdelrahmanorigin@gmail.com>
2025-02-04 10:29:01 +0200

    feat: add type command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::ExitCode;
+use std::{fmt, process::ExitCode};
 
 enum Command {
     Exit(u8),
     Echo(String),
-    Other,
+    Type(Result<String, String>),
+    Invalid(String),
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Command::Exit(_) => write!(f, "exit"),
+            Command::Echo(_) => write!(f, "echo"),
+            Command::Type(_) => write!(f, "type"),
+            Command::Invalid(arg) => write!(f, "{}", arg),
+        }
+    }
 }
 
 impl Command {
@@ -15,9 +27,20 @@ impl Command {
                 let code = code.parse::<u8>().map_err(|_| InvalidCommand)?;
                 Ok(Self::Exit(code))
             }
-            ["exit"] => Err(InvalidCommand),
+            // to help `Type` command
+            ["exit"] => Ok(Self::Exit(42)),
             ["echo", arg @ ..] => Ok(Self::Echo(arg.join(" "))),
-            _ => Ok(Self::Other),
+            ["type", arg @ ..] => {
+                let arg = Self::try_parse(arg);
+                match arg {
+                    Ok(command) => match command {
+                        Command::Invalid(invalid_type) => return Ok(Self::Type(Err(invalid_type))),
+                        _ => return Ok(Self::Type(Ok(command.to_string()))),
+                    },
+                    Err(_) => return Err(InvalidCommand),
+                }
+            }
+            _ => Ok(Self::Invalid(input_parts.join(" "))),
         }
     }
 }
@@ -41,12 +64,23 @@ fn main() -> ExitCode {
                 Command::Exit(0) => {
                     return ExitCode::SUCCESS;
                 }
-                Command::Exit(_) | Command::Other => {
+                Command::Exit(_) | Command::Invalid(_) => {
                     println!("{}: command not found", input.trim_end());
                     io::stdout().flush().unwrap();
                 }
                 Command::Echo(arg) => {
                     println!("{}", arg);
+                    io::stdout().flush().unwrap();
+                }
+                Command::Type(arg) => {
+                    match arg {
+                        Ok(valid_type) => {
+                            println!("{} is a shell builtin", valid_type);
+                        }
+                        Err(invalid_type) => {
+                            println!("{}: not found", invalid_type);
+                        }
+                    }
                     io::stdout().flush().unwrap();
                 }
             },
